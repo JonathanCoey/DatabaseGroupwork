96a8ce01918b9b38d828b6b37e8ab086fb378489
JonathanCoey <jonathan.coey@yahoo.co.uk>
2017-05-03 19:28:27 +0100

    Add files via upload

---
+++ DatabaseGroupwork/admin/includes/footer.php
@@ -0,0 +1,6 @@
+
+</div><br><br>
+<div class="col-md-12 text-center" id="footer">&copy 2017 Project by Daniel, Jonathan, Ryan, & Charlie</div>
+
+</body>
+</html>
---
+++ DatabaseGroupwork/admin/includes/navigation.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a href="DatabaseGroupwork/admin/store.php" class="navbar-brand">The Phone-Hub</a>
+        <ul class="nav navbar-nav">
+            <li><a href="brands.php">Brands</a> </li>
+
+            <!--
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                            <li><a href="#"></a></li>
+                    </ul>
+                </li> -->
+
+        </ul>
+    </div>
+</nav>
